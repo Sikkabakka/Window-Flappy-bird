0a0d685b375d6bf76dce44e7104f91659e30f2de
Bobles06 <Elias.gjerde@gmail.com>
2024-06-11 12:58:16 +0200

    Update bird.py

--- bird.py
+++ bird.py
@@ -1,11 +1,30 @@
 
+import tkinter
+import keyboard
+import threading
+import time
+# root = tkinter.Tk()
+# root.mainloop()
 
 
+def check_for_spacebar():
+    while True:
+        print("waiting for space")
+        keyboard.wait("space")
+        print("pressed")
+        
 class Bird:
     def __init__(self, size):
         self.size = size
-        
-        
+    position = 10
+    
+          
 bird = Bird(100)
-
-print(bird.size)
+thread = threading.Thread(target=check_for_spacebar, daemon=True).start(  )
+Running = True
+while Running:
+    bird.position -= 1  
+    time.sleep(1)
+    if (bird.position < 0):
+        Running = False 
+    print(bird.position) 
