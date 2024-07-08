dc4dc586f2257bb9b2e7c404b62a95ab46a6e773
Bobles06 <Elias.gjerde@gmail.com>
2024-07-09 00:08:57 +0200

    Fugl fixa!

--- birdscript.py
+++ birdscript.py
@@ -3,41 +3,67 @@ from tkinter import *
 import tkinter
 from PIL import Image, ImageTk
 from utils import *
-
+import time 
+import threading
+import keyboard
+import math
 
 
 class Bird:
     def __init__(self, size):
+        self.running = False
         self.size = size
         self.window = tkinter.Tk()
         self.window.title("Bird")
-        self.velocity = 10
-        monitor = getMonitorDimensions()
-        self.position = (int(monitor["width"]/10) , int(monitor["height"]/2)- self.size)
+        self.velocity = 0
+        self.flap_strength = -10
+        self.gravity = 0.5
+        self.monitor = getMonitorDimensions()
+        self.position = (int(self.monitor["width"]/10) , int(self.monitor["height"]/2)- self.size)
+        self.startime = 0
         
     def spawn(self):
        
         bilde = Image.open("bilder/flappy_bird.png").resize((self.size, self.size))
-        sprite = ImageTk.PhotoImage(bilde)
-        label = Label(self.window, image = sprite, width= self.size, height=self.size)
-        label.pack()
+        self.sprite = ImageTk.PhotoImage(bilde)
+        self.label = Label(self.window, image = self.sprite, width= self.size, height=self.size)
+        self.label.pack()
         
-        
+        self.window.bind("<space>", self.flap)
         self.window.geometry(f"{self.size}x{self.size}")
         self.window.geometry(f"+{self.position[0]}+{self.position[1]}")
-        self.window.mainloop()
-        
-    def flap(self):
-        self.velocity = -10
-        return
-
+        self.run()
        
         
-    def update(self):
-        self.position = (self.position[0], self.position[1]+self.velocity)
-        self.window.geometry(f"+{self.position[0]}+{self.position[1]}")
+        self.window.mainloop()
         
-     
-          
+    def flap(self, event = None):
+        print("flapped")
+        self.velocity = self.flap_strength
+  
+        
+    def update(self):
+       
+        self.velocity += self.gravity
+        self.velocity = min(self.velocity, 10)
+        
+        self.position = (self.position[0], math.floor(self.position[1]+ self.velocity))
+        self.window.geometry(f"+{self.position[0]}+{self.position[1]}")
+        self.startime = time.time()
+        
+        
+    def main_loop(self):
+        if self.running:
+            self.update()
+            self.check_out()
+            self.window.after(16, self.main_loop)
+    def check_out(self):
+        if (self.position[1] > self.monitor["height"]):
+            self.running = False
+            
 
+    def run(self):
+        self.running = True
+        self.main_loop()
+          
 
--- main.py
+++ main.py
@@ -4,26 +4,11 @@ import threading
 import time
 from birdscript import Bird
 
-def check_for_spacebar():
-    while True:
-        if (keyboard.is_pressed("space")):
-            bird.flap()
-            time.sleep(0.1)
-        
-        
+
         
 bird = Bird(100)
-threading.Thread(target=check_for_spacebar, daemon=True).start()
-def main_loop():
-    Running = True
-    while Running:
-        print("hwlloe")
-        bird.update()
-        
-        
-threading.Thread(target=main_loop, daemon=True).start()
-bird.spawn()
 
+bird.spawn()
 
 
 
